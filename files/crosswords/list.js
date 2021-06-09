ec64a85641ab966e354b31f351ed3a3dfb485c9a
mnebesny <michael@nebesny.ca>
2021-06-08 21:58:20 -0400

    Removed crosswords submodule

--- files/crosswords/list.js
+++
@@ -1,5 +0,0 @@
-let list = {
-    1: {name: "Law and Order", status: "available", date: "2021-01-21"},
-    2: {name: "Ice Cold", status: "unavailable"},
-    3: {name: "Reel Lucky", status: "unavailable"}
-} 
