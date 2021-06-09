a8c4510f5a1f1bc9d9b2effcda2d571fe9606918
mnebesny <michael@nebesny.ca>
2021-06-08 21:51:11 -0400

    Added crosswords submodule

---
+++ files/crosswords/list.js
@@ -0,0 +1,5 @@
+let list = {
+    1: {name: "Law and Order", status: "available", date: "2021-01-21"},
+    2: {name: "Ice Cold", status: "unavailable"},
+    3: {name: "Reel Lucky", status: "unavailable"}
+} 
